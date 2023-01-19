95a42aa862c84b4b38c998810339f7f518a79923
vtpqui <vtpqui@gmail.com>
2023-01-19 12:03:32 +0700

    Adding DATA folder to git

---
+++ DATA/GameData.js
@@ -0,0 +1,221 @@
+import {IMAGE_URL} from "./ImageUrl.js";
+
+export const GAME_DATA = [
+  {
+    id: 1,
+    uri: IMAGE_URL.drinkIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Một ly nước tùy em chọn nha em yêu',
+  },
+  {
+    id: 2,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 3,
+    uri: IMAGE_URL.kissIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Chúc mừng, em nhận được một nụ hôn sâu từ anh !',
+  },
+  {
+    id: 4,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 5,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 6,
+    uri: IMAGE_URL.moneyBag,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: '10K nha em '
+  },
+  {
+    id: 7,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 8,
+    uri: IMAGE_URL.hugIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Chúc mừng, em nhận được một cái ôm  từ anh !',
+  },
+  {
+    id: 9,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 10,
+    uri: IMAGE_URL.moneyBag,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: '50K nha em '
+  },
+  {
+    id: 11,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 12,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 13,
+    uri: IMAGE_URL.iceCreamIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Một cây kem nha em yêu',
+  },
+  {
+    id: 14,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 15,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 16,
+    uri: IMAGE_URL.datingIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Một buổi dating nha em yêu',
+  },
+  {
+    id: 17,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 18,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 19,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 20,
+    uri: IMAGE_URL.luggageIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Một chuyến du lịch nha em yêu',
+  },
+  {
+    id: 21,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 22,
+    uri: IMAGE_URL.shoppingIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Đi shopping nha em yêu',
+  },
+  {
+    id: 23,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 24,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 25,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 26,
+    uri: IMAGE_URL.driveLoverToWorkIcon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Anh chở đi làm nha em yêu',
+  },
+  {
+    id: 27,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 28,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 29,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 30,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 31,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 32,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 33,
+    uri: IMAGE_URL.iphone14Icon,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Iphone 14 nè, em mà quay trúng món này thì chúc em vui thiệt vui những ngày sau nè. Từ từ anh trả nhoa, yêu em lắm lắm !',
+  },
+  {
+    id: 34,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 35,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+  {
+    id: 36,
+    uri: IMAGE_URL.emptyBox,
+    defaultUri: IMAGE_URL.giftBoxIcon,
+    description: 'Hộp quà rỗng rồi nha em yêu, anh sẽ ôm hun em để đền bù nè',
+  },
+
+];
