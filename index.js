8fbeca19a9dfc6058f1577f1f0a45d460b72cdf3
vtpqui <vtpqui@gmail.com>
2023-01-19 11:58:30 +0700

    Adding .env file and engines property in package.json

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ const connectDb = async () => {
     }
 };
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
     console.log("Listening on port 5000");
 });
 
